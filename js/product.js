b74ac6a5c1b4285cde526942cee23e2d61b40770
martineleirvik <leirvikmartine@gmail.com>
2021-04-14 16:19:17 +0200

    Fetching products

---
+++ js/product.js
@@ -0,0 +1,28 @@
+const url = "http://www.flowerpower-ml.no/content-management-systems/wp-json/wc/store/products/?per_page=100";
+const jacketsContainer = document.querySelector(".jackets");
+
+async function getProducts() {
+    try{
+        const response = await fetch(url);
+        const getResults = await response.json();
+        createHTML(getResults);
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+}
+
+getProducts();
+
+
+function createHTML(jackets) {
+    jackets.forEach(function(product) {
+        jacketsContainer.innerHTML += 
+        `<div class="product">
+            <h2>${product.name}</h2>
+            <img src ="${product.images[0].src}" alt="${product.name}">
+            <p> Price: ${product.prices.currency_prefix}${product.prices.price}</p>
+        </div>`
+    })
+}
